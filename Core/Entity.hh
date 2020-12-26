e8b797262d8640d5f4196b4c6907cd8607480b31
Gaizgrol <gabrielizotongo@gmail.com>
2020-12-25 21:02:13 -0300

    +Separação de implementação de templates em arquivos separados, +Utilitário para mostrar mensagens estilizadas no terminal

--- Core/Entity.hh
+++ Core/Entity.hh
@@ -4,10 +4,7 @@
 // Global libraries
 #include <memory>
 #include <string>
-#include <type_traits>
 #include <unordered_map>
-
-#include "Serialization/type.hh"
 
 namespace Carrot
 {
@@ -44,47 +41,8 @@ namespace Carrot
             size_t layer;
             float position[2];
     };
-
-    template<typename T, typename... Args>
-    std::shared_ptr<T> Entity::addComponent( Args... args )
-    {
-        if ( std::is_base_of<Component, T>::value && !hasComponent<T>() )
-        {
-            auto newComponent = std::shared_ptr<T>( new T(args...) );
-            components.insert({ type<T>(), newComponent });
-            return newComponent;
-        }
-
-        return std::shared_ptr<T>( nullptr );
-    }
-    
-    template<typename T>
-    std::shared_ptr<T> Entity::getComponent()
-    {
-        auto component = components.find( type<T>() );
-        if ( component != components.cend() )
-            return std::static_pointer_cast<T>( component->second );
-        return std::shared_ptr<T>( nullptr );
-    }
-
-    template<typename T>
-    bool Entity::hasComponent()
-    {
-        auto component = components.find( type<T>() );
-        return component != components.cend();
-    }
-    
-    template<typename T>
-    std::shared_ptr<T> Entity::removeComponent()
-    {
-        if ( std::is_base_of<Component, T>::value )
-        {
-            auto k = getComponent<T>();
-            components.erase( type<T>() );
-            return k;
-        }
-        return std::shared_ptr<T>( nullptr );
-    }
 }
+
+#include "Entity.tcc"
 
 #endif
---
+++ Util/terminal.cc
@@ -0,0 +1,47 @@
+#include "terminal.hh"
+
+#include <initializer_list>
+#include <set>
+#include <string>
+
+namespace Carrot
+{
+    namespace Terminal
+    {
+        Modifier::Modifier( std::initializer_list<int> modifiers )
+        {
+            for ( auto mod : modifiers )
+                _modifiers.insert( std::to_string( mod ) );
+        }
+
+        std::string Modifier::operator()( const bool& reset )
+        {
+            return operator()("", reset);
+        }
+
+        std::string Modifier::operator()( const char* texto )
+        {
+            return operator()( std::string(texto) );
+        }
+        
+        std::string Modifier::operator()( const std::string& text, bool reset )
+        {
+            size_t i = 0;
+            size_t size = _modifiers.size();
+
+            std::string modString = "\033[";
+
+            for ( auto mod : _modifiers )
+            {
+                modString += mod;
+                if ( i < size - 1 )
+                    modString += ';';
+                i++;
+            }
+
+            modString += 'm' + text + ( reset ? "\033[0m" : "" );
+
+            return modString;
+        }
+    }
+}
